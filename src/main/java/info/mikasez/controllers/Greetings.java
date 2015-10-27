8e3db1ec6bcdd2fa1e572cabcf0fe4dff76dce62
MikaSez <mis.strator@gmail.com>
2015-10-27 09:23:56 +0100

    Adding changing states support

--- src/main/java/info/mikasez/controllers/Greetings.java
+++ src/main/java/info/mikasez/controllers/Greetings.java
@@ -3,13 +3,16 @@ package info.mikasez.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
    public class Greetings {
-        @RequestMapping("/evry")
-        public String greeting(Model model) {
+        @RequestMapping("/{name}")
+        public String greeting(@PathVariable String name, Model model) {
+            model.addAttribute("name", name);
+            System.out.println(name);
             return "greeting";
         }
     }
